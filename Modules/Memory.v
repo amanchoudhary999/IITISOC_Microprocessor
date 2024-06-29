faf5f0c63fb9f8e88f8f41d12dd53be15d56b02d
amanchoudhary999 <142250333+amanchoudhary999@users.noreply.github.com>
2024-06-29 18:05:23 +0530

    Update Memory.v

--- Modules/Memory.v
+++ Modules/Memory.v
@@ -4,9 +4,11 @@ module Memory(
     input clk,
     input write_en,
     input read_en,
+    input ir_en,
     input [7:0] add,
     input [7:0] data_in,
-    output reg [7:0] data_out    
+    output reg [7:0] data_out,
+    output reg [7:0] ir_out    
 );
 
     reg [7:0] memory [0:255];
@@ -14,10 +16,14 @@ module Memory(
     always @(posedge clk)
         begin
         data_out = 8'bxxxxxxxx;
+        ir_out = 8'bxxxxxxxx;
         if(write_en)
          memory[add] <= data_in;
         if(read_en)
          data_out <= memory[add];
+        if(ir_en)
+            ir_out <= memory[add+1];
         end
 endmodule
+
 
